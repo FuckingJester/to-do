50b8bb427d4c6dc09bceea3ca8b21a9dad4be097
Dima <dimon.belenok@gmail.com>
2023-01-20 22:12:17 +0200

    Create the add task function and make a checkboxes for comleted tasks

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
-const addBtn = document.querySelector('.content__add')
-const checkBtn = document.querySelectorAll('.item__check')
-const inputBtn = document.querySelector('.content__input')
-const delBtn = document.querySelector('.item__delete')
-const tasksBtn = document.querySelector('.main__tasks')
-const tasksAll = []
-
+const addBtn = document.querySelector('.content__add');
+const checkBtn = document.querySelector('.item__check');
+const inputBtn = document.querySelector('.content__input');
+const delBtn = document.querySelector('.item__delete');
+const tasksBtn = document.querySelector('.main__tasks');
+const taskItem = document.querySelectorAll('.tasks__item');
+const tasksAll = [];
 
 
 //CheckBox animation============================================================================
-checkBtn.forEach(check => {
-    check.addEventListener('click',(e) =>{
-        e.preventDefault()
-        check.classList.toggle('checked')
+tasksBtn.addEventListener('click', (e) => {
+        let targetItem = e.target
+        if (targetItem.closest('.item__check')) {
+            targetItem.closest('.item__check').classList.toggle('checked')
+            
+        }
     })
-})
 // Add task after click====================================================================
 addBtn.onclick = () => {
     const newTaskText = inputBtn.value
     if(newTaskText && isNotHaveTask(newTaskText, tasksAll)){
         addTask(newTaskText, tasksAll)
         inputBtn.value = ''
+        tasksRender(tasksAll)
     }
 }
 function addTask(text,arr){
@@ -38,11 +40,25 @@ function isNotHaveTask(text,arr){
         if(value.text === text){
             alert('Task already exist')
             isNotExist = false
+            inputBtn.value = ''
         }
     })
     return isNotExist
 }
-function removeTask(){
+//Add Task Into HTML==============================================================================
+function tasksRender(arr){
+    let htmlList = ''
 
+    arr.forEach((item) => {
+        const taskHtml = `                
+        <div id=${item.id} class="tasks__item item">
+            <div class="item__check"></div>
+            <div class="item__content">${item.text}</div>
+            <div class="item__delete"><a href="#">-</a> </div>
+        </div>
+        `
+        htmlList = htmlList + taskHtml
+    })
+    tasksBtn.innerHTML = htmlList
 }
 
